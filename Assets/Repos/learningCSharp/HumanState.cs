16fbc45adfd843cb083ab639694b4812e40312f2
echozs151 <echozs.151@gmail.com>
2017-08-20 16:05:59 +0300

    test

---
+++ Assets/Repos/learningCSharp/HumanState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanState : ZombieState {
+
+	// Use this for initialization
+	virtual public void LookAround()
+    {
+        GameObject[] Zombies = (GameObject[]);
+        GameObject.FindObjectsOfType(typeof(GameObject));
+        
+        foreach(GameObject go in Zombies)
+        {
+            ZombieState z - go.GetComponents<ZombieState
+        }
+          
+    }
+
+}
